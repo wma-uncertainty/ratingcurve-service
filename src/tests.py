e03ad94d0c2a6c8d41c45fd8670c0da74ad90405
thodson <thodson@usgs.gov>
2024-03-27 12:55:58 -0500

    Basic test

--- src/tests.py
+++ src/tests.py
@@ -2,14 +2,18 @@ from ratingcurve.ratings import PowerLawRating
 from ratingcurve import data
 
 
-def _precompile_ratingmodel():
+def test_rating(segments=1, n=100):
     """Precompile ratingmodel on test data"""
     # load tutorial data
     df = data.load('green channel')
 
     # initialize the model
-    powerrating = PowerLawRating(segments=1)
+    powerrating = PowerLawRating(segments=segments)
 
     # fit the model
     # TODO reduce iterations b/c we don't need a good fit
-    trace = powerrating.fit(q=df['q'], h=df['stage'], q_sigma=df['q_sigma'])
+    trace = powerrating.fit(
+        q=df['q'], h=df['stage'], q_sigma=df['q_sigma'], n=n, progressbar=False
+    )
+
+    return powerrating
